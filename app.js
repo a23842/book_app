013472bf923603eb37b156a0887eb7d990632477
Chenzf <372624240@qq.com>
2017-05-08 19:17:36 +0800

    第一次上传数据

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require('express');
 const app = express();
 
 //引入art-template模版引擎
-var	template	=	require('art-template');
-template.config('base','');
-template.config('extname','.html');
-app.engine('.html',template.__express);
-app.set('view engine','html');
-app.set('views',__dirname + '/views');
-app.use('/static',express.static('./views'));
+var template = require('art-template');
+template.config('base', '');
+template.config('extname', '.html');
+app.engine('.html', template.__express);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use('/static', express.static('./views'));
 
 //引入mongoose
 const db = require('mongoose');
@@ -29,28 +29,66 @@ var bookSchema = db.Schema({
     type: String
 });
 //模块映射
-var Book = db.model('Book',bookSchema);
+var Book = db.model('Book', bookSchema);
 
 
 
 app.use(express.static('./public'));
+//获取存储当前所有页面数量的数组
+function getPages(current, total) {
+    var pages = [current];
+    var left = current - 1;
+    var right = current + 1;
+    while (pages.length < 11 && (left > 0 || right <= total)) {
+        if (left>0) {
+            pages.unshift(left--);
+        }
+        if (right <= total) {
+            pages.push(right++);
+        }
+    }
+    return pages;
+}
+
 //书籍列表路由
-app.get('/list',(req,res)=>{
-    res.render('list');
-})
-app.get('/api/v1/book_data/:page',(req,res)=>{
-    //获取页数
-    var currentPage = req.params.page * 1;
-    //分页查询
+app.get('/list/:page?',(req,res)=>{
+    var currentPage = 1;
+    if(req.params.page){
+        currentPage = req.params.page*1;
+    }
+    //定义存储总页数
+    var totalPage = 0;
+    //每一页展示的书籍数
+    var perPage = 10;
+   Book.count({},function(err,total){
+    totalPage = Math.ceil(total/perPage);
+    //获取存储页面的数组
+    var pages = getPages(currentPage,totalPage); 
     Book.find({},function(err,data){
         if(err){
             console.log(err);
         }else{
-            res.json({status:200,books:data,page:currentPage+1});
+            res.render('list',{books:data,cPage:currentPage,totalPage:totalPage,pages:pages});
         }
-    }).limit(10).skip(currentPage*10);//限制显示条数
+    })
+   }) .limit(10).skip(currentPage*10);
 });
 
-app.listen(3000,()=>{
+
+
+app.get('/api/v1/book_data/:page', (req, res) => {
+    //获取页数
+    var currentPage = req.params.page * 1;
+    //分页查询
+    Book.find({}, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json({ status: 200, books: data, page: currentPage + 1 });
+        }
+    }).limit(10).skip(currentPage * 10);//限制显示条数
+});
+
+app.listen(3000, () => {
     console.log('Service running at port 3000.....')
 });
